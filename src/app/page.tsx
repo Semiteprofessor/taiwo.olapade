8a50dfc1c2a0c75ce17b67cd5c1f131ea3d3d97e
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-12-29 23:50:14 +0100

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-
+export const siteConfig = {
+  name: "Taiwo Olapade",
+  description: "I am a passionate Software Developer.",
+  ogImage: "",
+  url: "http://taiwoolapade.vercel.app",
+};
 export default function Home() {
   return <div>Hello !!!</div>;
 }
