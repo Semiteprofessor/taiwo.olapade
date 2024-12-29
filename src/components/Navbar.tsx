67349d0126c550f709da699888d431a413ee263b
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2024-12-30 00:12:12 +0100

    Commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,50 @@
 "use client";
+import { cn } from "@/lib/utils";
+import {
+  Briefcase,
+  FolderGit2,
+  GraduationCap,
+  HomeIcon,
+  Mail,
+  MoreHorizontal,
+  User,
+} from "lucide-react";
 
-import {cn} from "@/lib/utils"
+const Navbar = () => {
+  const navLinks = [
+    { title: "Home", icon: <HomeIcon className="h-full w-full" />, href: "/" },
+    {
+      title: "About",
+      icon: <User className="h-full w-full" />,
+      href: "/about",
+    },
+    {
+      title: "Skills",
+      icon: <Briefcase className="w-full h-full" />,
+      href: "/skills",
+    },
+    {
+      title: "Education",
+      icon: <GraduationCap className="w-full h-full" />,
+      href: "/education",
+    },
+    {
+      title: "Projects",
+      icon: <FolderGit2 className="w-full h-full" />,
+      href: "/projects",
+    },
+    {
+      title: "Contact",
+      icon: <Mail className="w-full h-full" />,
+      href: "/contact",
+    },
+    {
+      title: "More",
+      icon: <MoreHorizontal className="w-full h-full" />,
+      href: "/more",
+    },
+  ];
+  return <div></div>;
+};
+
+export default Navbar;
