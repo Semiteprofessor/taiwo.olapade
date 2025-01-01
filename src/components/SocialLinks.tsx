618a8b4a6c634724950ebeef802ee418f6508532
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-01-01 23:40:56 +0100

    Commit

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,5 +1,7 @@
 import { ExternalLink, Facebook, Linkedin, Twitter } from "lucide-react";
 import React from "react";
+import FramerWrapper from "./animation/FramerWrapper";
+import Link from "next/link";
 
 const SocialLinks = () => {
   const links = [
@@ -24,7 +26,26 @@ const SocialLinks = () => {
       icon: <ExternalLink />,
     },
   ];
-  return <div></div>;
+  return (
+    <>
+      {links.map((item, index) => {
+        const timing = 0.08 + index * 0.125;
+        return (
+          <FramerWrapper key={index} delay={timing} y={50}>
+            <Link
+              target="black"
+              key={index}
+              href={item.url}
+              rel="noopener noreferrer"
+              className="hover:text-primary transition duration-300"
+            ></Link>
+
+            {item.icon}
+          </FramerWrapper>
+        );
+      })}
+    </>
+  );
 };
 
 export default SocialLinks;
