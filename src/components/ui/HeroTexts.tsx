b6b3dd85f7a7637c37d5948ed8b1751a352a62b8
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-01-01 23:20:30 +0100

    Commit

--- src/components/ui/HeroTexts.tsx
+++ src/components/ui/HeroTexts.tsx
@@ -1,11 +1,15 @@
-
+import TextRotator from "./TextRotator";
 
 const HeroTexts = () => {
   return (
     <>
-      <h3></h3>
+      <h3 className="font-poppins text-2xl max-sm:text-xl">My name is</h3>
+      <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl">
+        Taiwo Olapade
+      </h1>
+      <TextRotator />
     </>
-  )
-}
+  );
+};
 
-export default HeroTexts
+export default HeroTexts;
