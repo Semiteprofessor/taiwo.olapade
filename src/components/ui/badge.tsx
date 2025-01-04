2d98d6045d6080d72c9867ebf2410ed78978ea9d
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-01-04 11:03:24 +0100

    Commit

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,4 +1,6 @@
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
+import FramerWrapper from "../animation/FramerWrapper";
+import { cn } from "@/lib/utils";
 
 const badgeVariant = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xm font-semibold transition-colors focus:outline-none focus:ring-2 focud:ring-ring focus:ring-offset-2",
@@ -18,3 +20,17 @@ const badgeVariant = cva(
     },
   }
 );
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariant> {}
+
+const Badge = ({ className, variant, ...props }: BadgeProps) => {
+  return (
+    <FramerWrapper>
+      <div className={cn(badgeVariant({ variant, className }))} {...props} />
+    </FramerWrapper>
+  );
+};
+
+export { Badge, badgeVariant };
