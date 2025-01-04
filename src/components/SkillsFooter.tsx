863f982cb4d9fb3a748ff72082632b3f53e1c136
Taiwo Olapade <44630802+Semiteprofessor@users.noreply.github.com>
2025-01-04 21:15:30 +0100

    Commit

---
+++ src/components/SkillsFooter.tsx
@@ -0,0 +1,27 @@
+import { div } from "framer-motion/client";
+import Image from "next/image";
+import React from "react";
+
+interface MyComponentProps {
+  items: Array<{ alt: string; img: any }>;
+}
+const SkillsFooter: React.FC<MyComponentProps> = ({ items }) => {
+  return (
+    <>
+      {items &&
+        items.map((val, idx) => {
+          return (
+            <div className="p-4" key={idx}>
+              <Image
+                src={val?.img}
+                alt={val.alt}
+                className="rounded-full w-auto max-h-20"
+              />
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+export default SkillsFooter;
