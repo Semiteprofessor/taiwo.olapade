d182dec582e886683233b2f8d3d410953182b532
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-10 14:30:46 +0100

    Commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,21 +6,19 @@ import { projects } from "@/lib/utils/constants";
 import { Layers } from "lucide-react";
 
 const Project = () => {
+
   return (
     // PROJECT PAGE
     <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
-      {/* Header Badge */}
-      <Badge className="gap-2">
+      <Badge className=" gap-2">
         <Layers className="h-5 w-5" />
         Projects
       </Badge>
-
-      {/* Heading + Intro */}
       <div className="flex flex-col gap-3">
         <Heading>My Projects</Heading>
         <FramerWrapper y={0} x={200}>
-          <p className="font-poppins text-lg w-full text-primary max-sm:text-base">
-            I love to build cool projects. Here, you&#x27;ll find a curated
+          <p className=" font-poppins text-lg w-full text-primary max-sm:text-base">
+            I love to Build Cool Projects. Here, you&#x27;ll find a curated
             collection of my creative endeavors and technical projects. Each
             piece represents a journey of innovation, problem-solving, and
             continuous learning. Feel free to explore this showcase of my
@@ -29,11 +27,10 @@ const Project = () => {
         </FramerWrapper>
       </div>
 
-      {/* Projects Grid */}
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((val, indx) => (
-          <ProjectCards key={indx} value={val} num={indx} />
-        ))}
+      <div className="w-full flex flex-row flex-wrap gap-3 max-lg:flex-col">
+        {projects.map((val, indx) => {
+          return <ProjectCards key={indx} value={val} num={indx} />;
+        })}
       </div>
     </div>
   );
