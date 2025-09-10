de3d4c03783843fbb1845f2132fd8f1321afebab
Taiwo Olapade <semiteprofessor@gmail.com>
2025-09-10 14:28:32 +0100

    Commit

--- src/components/DownloadResumeBtn.tsx
+++ src/components/DownloadResumeBtn.tsx
@@ -4,7 +4,9 @@ import HackerBtn from "./animation/HackerBtn";
 const DownloadResumeBtn = () => {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <HackerBtn label="Download Resume" />
+      <a href="/resume.pdf" download="Taiwo_Olapade.pdf">
+        <HackerBtn label="Download Resume" />
+      </a>
     </div>
   );
 };
